bed3429a7e72311c5ebe1a43fb848ecbe686404a
Sergio Prieto <sergio@yieldstudio.fr>
2022-09-19 18:44:41 +0200

    4 add progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {createInertiaApp} from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
 
 import '../css/app.css';//import the app css
+
+InertiaProgress.init({ color: 'red', showSpinner: true})
 
 createInertiaApp({
     resolve: async (name) => {
