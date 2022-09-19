db8a3a17530fc7b82ddcd2c4de02b8f70cf8805a
Sergio Prieto <sergio@yieldstudio.fr>
2022-09-19 16:34:40 +0200

    2 use vit and implement react and tailwind

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Home () {
+    return <div>
+        <h1 className={'bg-green-600'}>Hello world</h1>
+    </div>
+}
+
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default ({ command }) => ({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'fake_dir_so_nothing_gets_copied',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
+
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        react(),
+        //add blade plugin to reload page on change blade files
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
 });
