76bd01a7f7a507d458de450eb10846f996f506fe
Sergio Prieto <sergio@yieldstudio.fr>
2022-09-20 17:14:30 +0200

    17 useForm inertia helper

--- resources/js/Pages/CreateUser.jsx
+++ resources/js/Pages/CreateUser.jsx
@@ -1,15 +1,13 @@
-import React, {useState} from 'react';
-import {Head, usePage} from "@inertiajs/inertia-react";
-import {Inertia} from "@inertiajs/inertia";
+import React from 'react';
+import {Head, useForm} from "@inertiajs/inertia-react";
 
 const CreateUser = () => {
-    const {props} = usePage()
 
-    const [form, setForm]= useState({name:'', email: '', password: ''})
+    const {data, setData, post, processing, errors} = useForm({name:'', email: '', password: ''})
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        Inertia.post('/users', form)
+        post('/users')
     }
 
     return (
@@ -27,14 +25,14 @@ const CreateUser = () => {
                         </label>
 
                         <input
-                            value={form.name}
-                            onChange={event => setForm((prevState) => ({...prevState, name: event.target.value}))}
+                            value={data.name}
+                            onChange={event => setData('name', event.target.value)}
                             className='border border-gray-300 p-2 w-full'
                             name='name'
                             id='name'
                             required
                             type="text"/>
-                        {props.errors.name && <div className='text-xs text-red-500'>{props.errors.name}</div>}
+                        {errors.name && <div className='text-xs text-red-500'>{errors.name}</div>}
                     </div>
 
                     <div className='mt-6'>
@@ -43,14 +41,14 @@ const CreateUser = () => {
                         </label>
 
                         <input
-                            value={form.email}
-                            onChange={event => setForm((prevState) => ({...prevState, email: event.target.value}))}
+                            value={data.email}
+                            onChange={event => setData('email', event.target.value)}
                             className='border border-gray-300 p-2 w-full'
                             name='email'
                             id='email'
                             required
                             type="email"/>
-                        {props.errors.email && <div className='text-xs text-red-500'>{props.errors.email}</div>}
+                        {errors.email && <div className='text-xs text-red-500'>{errors.email}</div>}
 
                     </div>
 
@@ -60,19 +58,26 @@ const CreateUser = () => {
                         </label>
 
                         <input
-                            value={form.password}
-                            onChange={event => setForm((prevState) => ({...prevState, password: event.target.value}))}
+                            value={data.password}
+                            onChange={event => setData('password', event.target.value)}
                             className='border border-gray-300 p-2 w-full'
                             name='password'
                             id='password'
                             required
                             type="password"/>
-                        {props.errors.password && <div className='text-xs text-red-500'>{props.errors.password}</div>}
+                        {errors.password && <div className='text-xs text-red-500'>{errors.password}</div>}
 
                     </div>
 
                     <div className="mt-6">
-                        <button onClick={handleSubmit} type='submit' className='bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500'>
+                        <button onClick={handleSubmit} disabled={processing} type='submit' className='flex bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500'>
+                            {processing && <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                <path className="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>}
                             Submit
                         </button>
                     </div>
