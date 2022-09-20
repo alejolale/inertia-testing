8afd25da87fd659fcbd29b75c0b175a869b5e949
Sergio Prieto <sergio@yieldstudio.fr>
2022-09-20 09:51:38 +0200

    7 active links

--- resources/js/Components/Nav.jsx
+++ resources/js/Components/Nav.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import {Link} from "@inertiajs/inertia-react";
+import {Link, usePage} from "@inertiajs/inertia-react";
+import clsx from "clsx";
 
 const Nav = () => {
+
+    // Two approaches to get page props
+    const {url, component} = usePage()
+
+    console.log(url)
     return (
         <header>
             <nav>
-                <Link href='/'>Accueil</Link>
-                <Link href='/demo'>Demo</Link>
-                <Link href='/test'>Test</Link>
+                {/* We can use the component name to compare or url but attention to composed urls like /demo?hey=asjzoiio */}
+                <Link href='/' className={clsx(component === 'Home' && 'text-red-500 underline')}>Accueil</Link>
+                <Link href='/demo' className={clsx(url.startsWith('/demo') && 'text-red-500 underline')}>Demo</Link>
+                <Link href='/test' className={clsx(url === '/test' && 'text-red-500 underline')}>Test</Link>
                 <Link href='/logout' method='post' as='button' data={{foo: 'Log out information'}} >Logout</Link>
             </nav>
         </header>
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from "../Layout";
+import Nav from "../Components/Nav";
 
 function Home ({name}) {
     return <div>
+        <Nav />
         <h1 className={'bg-green-600'}>Hello {name}</h1>
     </div>
 }
 
-Home.layout = (page) => <Layout children={page} />
+//Home.layout = (page) => <Layout children={page} />
 
 export default Home;
